173c4db2fdebfb220613d4d1468ef3a917a2524f
Jay Sharma <sharmajaym2007@gmail.com>
2021-06-21 23:08:37 -0500

    Initial Spring Boot project

---
+++ src/main/java/com/plantaway/plantaway/PlantawayApplication.java
@@ -0,0 +1,13 @@
+package com.plantaway.plantaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantawayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantaway/plantaway/PlantawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantaway.plantaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
